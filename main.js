5cc2709925aab67c1aedfe324ec4bb9286f571eb
jannesoik <janne.soikkeli@edu.turkuamk.fi>
2019-06-15 11:35:36 +0300

    la-aamu, tekstit nappien sisällä toimii

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var coll = document.getElementsByClassName("collapsible");
+var text = document.getElementsByClassName("text");
 var i;
+console.log(text.length);
 
-for (i = 0; i < coll.length; i++) {
+for (i = 0; i < text.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    this.innerHTML=content.innerHTML;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
